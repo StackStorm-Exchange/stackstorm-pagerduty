792e66dc4b299ad7de4a164790cbdaf860e98983
Jon Middleton <jjm@ixtab.org.uk>
2018-10-08 20:33:06 +0100

    Add delete_user action

---
+++ actions/delete_user.py
@@ -0,0 +1,38 @@
+from lib.action import PagerDutyAction
+from pypd.errors import BadRequest
+
+
+class DeleteUser(PagerDutyAction):
+    def run(self, email):
+        """
+        Delete a user from PagerDuty (`or raise an error if not possible)
+
+        :param email: The
+        :return: (success (bool), results (obj).
+        """
+        success = True
+        results = {
+            "user_id": None,
+            "error": None
+        }
+
+        user_id = None
+
+        users = self.pager.User.find(email=email)
+
+        if users:
+            if len(users) == 1:
+                results['user_id'] = users[0].id
+                try:
+                    users[0].remove()
+                except BadRequest as e:
+                    success = False
+                    results['error'] = str(e)
+            else:
+                results['error'] = "Found {} users!".format(len(users))
+                success = False
+        else:
+            results['error'] = "Found {} users!".format(len(users))
+            success = False
+
+        return success, results
