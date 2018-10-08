e5f5c53ce6df512887c5ec87dddedbaf4be8eb1f
Jon Middleton <jjm@ixtab.org.uk>
2018-10-08 21:14:36 +0100

    Add missing config

--- tests/test_action_create_user.py
+++ tests/test_action_create_user.py
@@ -4,6 +4,7 @@ from st2tests.base import BaseActionTestCase
 
 class PagerDuytyCreateUserActionTestCase(BaseActionTestCase):
     action_cls = CreateUser
+    full_config = {'api_key': 'abc1234', 'service_key': 'abc1234'}
 
     def test_run_is_instance(self):
         action = self.get_action_instance(self.full_config)
