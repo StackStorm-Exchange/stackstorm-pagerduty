21ef89443013236c62427c6c231b4c94826ccf00
Jon Middleton <jjm@ixtab.org.uk>
2018-10-08 21:28:20 +0100

    Add basic unittests for existing actions

---
+++ tests/test_action_open_incidents.py
@@ -0,0 +1,11 @@
+from open_incidents import OpenIncident
+from st2tests.base import BaseActionTestCase
+
+
+class PagerDuytyOpenIncidentActionTestCase(BaseActionTestCase):
+    action_cls = OpenIncident
+    full_config = {'api_key': 'abc1234', 'service_key': 'abc1234'}
+
+    def test_run_is_instance(self):
+        action = self.get_action_instance(self.full_config)
+        self.assertIsInstance(action, self.action_cls)
---
+++ tests/test_action_resolve_incident.py
@@ -0,0 +1,11 @@
+from resolve_incident import ResolveIncident
+from st2tests.base import BaseActionTestCase
+
+
+class PagerDuytyResolveIncidentActionTestCase(BaseActionTestCase):
+    action_cls = ResolveIncident
+    full_config = {'api_key': 'abc1234', 'service_key': 'abc1234'}
+
+    def test_run_is_instance(self):
+        action = self.get_action_instance(self.full_config)
+        self.assertIsInstance(action, self.action_cls)
