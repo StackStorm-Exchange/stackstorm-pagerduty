1dd44386e0fc8a350e03543b8e5020a2454bc8c3
Jon Middleton <jjm@geeky-and-blonde.me.uk>
2018-10-09 09:30:36 +0100

    Remove white space

--- tests/test_action_list_users.py
+++ tests/test_action_list_users.py
@@ -5,11 +5,10 @@ from st2tests.base import BaseActionTestCase
 
 
 class PdUser(object):
-    
     def __init__(self, pd_id, email):
         self._pd_id = pd_id
         self._email = email
-        
+
     @property
     def id(self):
         return self._pd_id
@@ -26,7 +25,7 @@ class PagerDuytyListUsersActionTestCase(BaseActionTestCase):
     def test_run_is_instance(self):
         action = self.get_action_instance(self.full_config)
         self.assertIsInstance(action, self.action_cls)
-        
+
     def test_run_list_user_empty(self):
         expected = []
 
@@ -51,7 +50,7 @@ class PagerDuytyListUsersActionTestCase(BaseActionTestCase):
 
         result = action.run()
         self.assertEqual(result, expected)
-     
+
     def test_run_list_user_multiple(self):
         expected = [
             {'id': 'PD1234', 'email': 'bob@example.com'},
