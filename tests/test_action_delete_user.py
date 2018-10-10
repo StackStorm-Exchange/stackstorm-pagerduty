a80414bde2c51efa32108c81ca880b2e9262d6c5
Jon Middleton <jjm@ixtab.org.uk>
2018-10-10 21:11:02 +0100

    Test a successful user removal

--- tests/test_action_delete_user.py
+++ tests/test_action_delete_user.py
@@ -2,6 +2,25 @@ from list_users import ListUsers
 from st2tests.base import BaseActionTestCase
 
 
+class PdUser(object):
+    @property
+    def id(self):
+        return 'PD1234'
+
+    @property
+    def email(self):
+        return 'bob@example.com'
+
+    @staticmethod
+    def remove():
+        return True
+
+
+class BadRequest(object):
+    pass
+
+
+
 class PagerDuytyCreateUserActionTestCase(BaseActionTestCase):
     action_cls = ListUsers
     full_config = {'api_key': 'abc1234', 'service_key': 'abc1234'}
@@ -9,3 +28,16 @@ class PagerDuytyCreateUserActionTestCase(BaseActionTestCase):
     def test_run_is_instance(self):
         action = self.get_action_instance(self.full_config)
         self.assertIsInstance(action, self.action_cls)
+
+    def test_run_removal_success(self):
+        expected = {"user_id": PD1234, "error": None}
+
+        action = self.get_action_instance(self.full_config)
+
+        action.pager.User.find = MagicMock(
+            return_value=[PdUser()]
+        )
+
+        success, result = action.run()
+        self.assertTrue(success)
+        self.assertEqual(result, expected)
