aa97e3fe1f59305d1c7de21fadab19f752288ae8
Jon Middleton <jjm@ixtab.org.uk>
2018-10-10 22:19:54 +0100

    Add test_run_create_success test

--- tests/test_action_create_user.py
+++ tests/test_action_create_user.py
@@ -2,6 +2,20 @@ from create_user import CreateUser
 from st2tests.base import BaseActionTestCase
 
 
+class PdUser(object):
+    @property
+    def id(self):
+        return 'PD1234'
+
+    @property
+    def email(self):
+        return 'bob@example.com'
+
+    @staticmethod
+    def json():
+        return {"id": self.id, "email": self.email}
+
+
 class PagerDuytyCreateUserActionTestCase(BaseActionTestCase):
     action_cls = CreateUser
     full_config = {'api_key': 'abc1234', 'service_key': 'abc1234'}
@@ -9,3 +23,21 @@ class PagerDuytyCreateUserActionTestCase(BaseActionTestCase):
     def test_run_is_instance(self):
         action = self.get_action_instance(self.full_config)
         self.assertIsInstance(action, self.action_cls)
+
+    def test_run_create_success(self):
+        expected = {"user_id": "PD1234", "email": 'bob@example.com'}
+
+        action = self.get_action_instance(self.full_config)
+
+        action.pager.User.find = MagicMock(
+            return_value=PdUser()
+        )
+
+        (success, result) = action.run(
+            name="Bob",
+            email='bob@example.com',
+            role="user",
+            job_title="Chef cook and bottle washer"
+            )
+        self.assertTrue(success)
+        self.assertEqual(result, expected)
