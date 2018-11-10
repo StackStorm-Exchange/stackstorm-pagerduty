792bde12a7b9ba91c07a7b25b796e2a122dfb3ad
Ian Price <iprice@tableau.com>
2018-11-09 18:46:06 -0800

    updates

--- actions/action.py
+++ actions/action.py
@@ -1,5 +1,8 @@
 from lib.base import PdBaseAction
 
+""" This action can be used as a shortcut for the methods .fetch() .find() and .delete()
+	They follow a strict set of logic that is easily repeatable.
+"""
 
 class PdAction(PdBaseAction):
 	""" Pagerduty run action
@@ -15,30 +18,33 @@ class PdAction(PdBaseAction):
 
 		#Well known pypd methods in pypd.entity
 		if method == 'fetch':
-			#We need to know the id of the resource we are fetching. Define 'resource_id' in your action
-			if not kwargs.get('resource_id', None):
-				return (False, "resource_id is a required field for fetch()")
-			resource_id = str(kwargs.pop('resource_id'))
-			return (True, self.fetch(entity=entity, _id=resource_id, **kwargs))
+			#We need to know the id of the resource we are fetching. Define '_id' in your action
+			if not kwargs.get('_id', None):
+				return (False, "_id is a required field for fetch()")
+			_id = str(kwargs.pop('_id'))
+			return (True, self.fetch(entity=entity, _id=_id, **kwargs))
 
 		elif method == 'find':
 			return (True, self.find(entity=entity, **kwargs))
+
+		elif method == 'delete':
+			#We need to know the id of the resource we are deleting. Define '_id' in your action
+			if not kwargs.get('_id', None):
+				return (False, "_id is a required field for delete()")
+			_id = str(kwargs.pop('_id'))
+			return (True, self.delete(entity=entity, id=_id, **kwargs))
 
 		elif method == 'create':
 			#We need to know the email of the user making the resource
 			if not kwargs.get('from_email', None):
 				return (False, "from_email is a required field for create()")
+			if not kwargs.get('data', None):
+				return (False, "_id is a required field for fetch()")
+
+			#data should be 
+			data = str(kwargs.pop('data'))
 			from_email = str(kwargs.pop('from_email'))
-			return (True, self.create(entity=entity, from_email=from_email, **kwargs))
+			return (True, self.create(entity=entity, from_email=from_email, payload=data, **kwargs))		
 
-		elif method == 'delete':
-			#We need to know the id of the resource we are deleting. Define 'resource_id' in your action
-			if not kwargs.get('resource_id', None):
-				return (False, "resource_id is a required field for delete()")
-			resource_id = str(kwargs.pop('resource_id'))
-			return (True, self.delete(entity=entity, _id=resource_id, **kwargs))
-
-#Next up... Variable methods!
-		# else:
-		# 	#many entities have custom methods that require specific fields, if 'id' is significant, pass 'resource_id' and it will be handled
-		# 	return (True, self.other(entity=entity, method=method, **kwargs))
+		#other post and put based methods
+		else:
--- actions/lib/base.py
+++ actions/lib/base.py
@@ -7,7 +7,8 @@ from st2common.runners.base_action import Action
 class PdBaseAction(Action):
     """ Base Pagerduty class for all actions
     """
-
+    if entity is None:
+        raise InvalidArguments(entity)
 
     def __init__(self, config):
         """ init method, run at class creation 
@@ -27,8 +28,6 @@ class PdBaseAction(Action):
     def fetch(self, entity=None, _id=None, **kwargs):
         """ base fetch() method defined in pypd.entity.fetch() usable by most entities
         """
-        if entity is None:
-            raise InvalidArguments(entity)
         if _id is None:
             raise InvalidArguments(_id)
         fetch = getattr(self.pd, entity).fetch(id=_id, **kwargs)
@@ -39,11 +38,9 @@ class PdBaseAction(Action):
     def find(self, entity=None, **kwargs):
         """ base find() method defined in pypd.entity.find() usable by all entities
         """
-        if entity is None:
-            raise InvalidArguments(entity)
         if not 'maximum' in kwargs:
             #if maximum was ommited from the action, or didn't have a default, make sure we don't have a rediculous response
-            kwargs['maximum'] = 10
+            kwargs['maximum'] = 25
 
         find = getattr(self.pd, entity).find(**kwargs): 
         found = []
@@ -53,27 +50,9 @@ class PdBaseAction(Action):
         #use pypd method entity.json to return the entity as json
         return found
 
-
-    def create(self, entity=None, from_email=None, **kwargs):
-        """ base create() method defined in pypd.entity.create() usable by most entities
-        """
-        if entity is None:
-            raise InvalidArguments(entity)
-        if from_email is None:
-            raise InvalidArguments(from_email)
-        payload = {}
-        for k, v in kwargs.iteritems():
-            payload[k] = v
-        create = getattr(self.pd, entity).create(data=json.dumps(payload), from_email=from_email)
-        #use pypd method entity.json to return the entity as json
-        return create.json
-
-
     def delete(self, entity=None, _id=None, **kwargs):
         """ base delete() method defined in pypd.entity.delete() usable by most entities
         """
-        if entity is None:
-            raise InvalidArguments(entity)
         if _id is None:
             raise InvalidArguments(_id)
         delete = getattr(self.pd, entity).delete(id=_id, **kwargs)
@@ -83,40 +62,47 @@ class PdBaseAction(Action):
             #use pypd method entity.json to return the entity as json
             return delete.json
 
-#Next up... Variable methods!
-    # def other(self, entity=None, method=None, **kwargs):
-    #     """ method for variable entity operations
-    #     """ 
-    #     if entity is None:
-    #         raise InvalidArguments(entity)    
-    #     if method is None:
-    #         raise InvalidArguments(method)
-    #     data = {}
-    #     if kwargs.get('resource_id', None):
-    #         resource_id = str(kwargs.pop('resource_id'))
-    #         #if resource_id is present it means that we need to run the method against an existing resource, look it up
-    #         resource = getattr(self.pd, entity).fetch(id=resource_id)
+    def create(self, entity=None, from_email=None, **kwargs):
+        """ base create() method defined in pypd.entity.create() usable by most entities
+        """
+        if from_email is None:
+            raise InvalidArguments(from_email)
 
-    #     if kwargs.get('from_email', None):
-    #         from_email = str(kwargs.pop('from_email'))
-    #         data['from_email'] = from_email
+        payload = json.dumps(kwargs)
 
-    #     payload = {}
-    #     for k, v in kwargs.iteritems():
-    #         payload[k] = v  
-    #     data['data'] = json.dumps(payload) #assign payload as a string value to a key
-    #     parameters = ''.join('{0}{1}'.format(key, val) for key, val in data.iteritems()) 
+        create = getattr(self.pd, entity).create(data=payload, from_email=from_email, **kwargs)
+        #use pypd method entity.json to return the entity as json
+        return create.json
 
-    #     if resource_id:
+    def send_payload(self, entity=None, method=None, _id=None, payload=None, **kwargs):
+        """ base POST method to handle other POST type methods
+            Make sure to use the PD API reference to determine if your action needs a `from` (use action parameter `from_email`)
 
-    #         result = setattr(resource, method, )
+            Payload should be a JSON string with the keys and values as defined from the PD API reference
+            For some reason all the examples will have one extra top level key that should be omitted.
+            For example, in Teams/put_teams_id the payload ('team') example is
+                {
+                  "team": {
+                    "type": "team",
+                    "name": "Engineering",
+                    "description": "The engineering team"
+                  }
+                }
+            Where as the actual payload should be
+                {
+                    "type": "team",
+                    "name": "Engineering",
+                    "description": "The engineering team"
+                }
+        """
+        #check for required fields
+        if not _id:
+            raise InvalidArguments(_id)
+        if not payload:
+            raise InvalidArguments(payload)
 
+        post = getattr(getattr(self.pd, entity), ("%s(id=%s, payload=%s, %s" % method, _id, payload, **kwargs))
+        #use pypd method entity.json to return the entity as json
+        return post.json
 
-# class Error(Exception):
-#     pass
-
-
-# class MissingArguments(Error):
-#     """ arguments that are required were missing or None
-#     """
-#     def __init__(self, *args):
+    put = post #if someone assumes they should use put(), its really the same logic as post()
