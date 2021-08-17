e270fc58b91e66151caa13ccbdc2b6387158922e
Yakir Oren <yakiroren@gmail.com>
2021-08-17 12:03:16 +0300

    sdk update and refactor.

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	openapi_sdk "github.com/blinkops/blink-openapi-sdk/plugin"
 	plugin_sdk "github.com/blinkops/blink-sdk"
+	"github.com/blinkops/blink-sdk/plugin"
 	"github.com/blinkops/blink-sdk/plugin/connections"
 	log "github.com/sirupsen/logrus"
+	"net/http"
 )
 
 const (
@@ -12,8 +15,6 @@ const (
 )
 
 func main() {
-	pagerdutyOpenApi := "pagerduty-openapi.yaml"
-	maskFile := "mask.yaml"
 	connectionTypes := map[string]connections.Connection{
 		pagerduty: {
 			Name:      pagerduty,
@@ -22,7 +23,30 @@ func main() {
 		},
 	}
 
-	pagerdutyPlugin, err := openapi_sdk.NewOpenApiPlugin(pagerduty, pagerduty, []string{pagerduty}, connectionTypes, pagerdutyOpenApi, maskFile)
+	metadata := openapi_sdk.PluginMetadata{
+		Name:           pagerduty,
+		Provider:       pagerduty,
+		MaskFile:       "mask.yaml",
+		OpenApiFile:    "pagerduty-openapi.yaml",
+		Tags:           []string{pagerduty},
+		HeaderPrefixes: map[string]string{"AUTHORIZATION":"Token token="},
+	}
+	checks := openapi_sdk.PluginChecks{
+		TestCredentialsFunc:
+		func(ctx *plugin.ActionContext) (*plugin.CredentialsValidationResponse, error) {
+			value, err := AuthTest(ctx)
+
+			return &plugin.CredentialsValidationResponse{
+				AreCredentialsValid:   value,
+				RawValidationResponse: err,
+			}, nil
+		},
+
+		ValidateResponse: Validate,
+	}
+
+
+	pagerdutyPlugin, err := openapi_sdk.NewOpenApiPlugin(connectionTypes, metadata, checks)
 
 	if err != nil {
 		log.Error("Failed to create pagerduty integration: ", err)
@@ -35,4 +59,32 @@ func main() {
 		log.Error("Failed to start pagerduty integration: ", err)
 		panic(err)
 	}
+}
+
+func AuthTest(ctx *plugin.ActionContext) (bool,[]byte) {
+
+	var data map[string]interface{}
+
+	req, _ := http.NewRequest(http.MethodGet, "https://api.pagerduty.com/services", nil)
+
+	response, err := openapi_sdk.ExecuteRequest(ctx, req, pagerduty, map[string]string{"AUTHORIZATION":"Token token="},30 )
+	if err != nil {
+		return false, []byte(err.Error())
+	}
+
+	json.Unmarshal(response, &data)
+
+	return Validate(data)
+
+}
+
+func Validate(json map[string]interface{})(bool, []byte){
+	m := json["error"]
+
+	if m != nil {
+		m := m.(map[string]interface{})
+		return false, []byte(m["message"].(string))
+	}
+
+	return true, nil
 }
